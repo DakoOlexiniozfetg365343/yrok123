413b915cc00b94a19f9fbb60e6871eaaa24405c6
Dako <sashadakopro@gmail.com>
2023-04-22 09:31:49 +0300

    add new import

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print ("123")
+import requests
+from bs4 import BeautifulSoup
+
+
+
+URL = ("https://coinmarketcap.com/")
+
+r = requests.get(URL)
+
+data = r.text
+
+print(data)
+soup = BeautifulSoup (data, 'html.parser')
+
+
