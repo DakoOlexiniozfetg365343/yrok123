038dd40d1c548798b84d21d693e8e67dc57e7858
Dako <sashadakopro@gmail.com>
2023-04-22 09:52:51 +0300

    add "us"

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 
-
-
-URL = ("https://coinmarketcap.com/")
+URL = "https://coinmarketcap.com/"
 
 r = requests.get(URL)
 
 data = r.text
 
-print(data)
 soup = BeautifulSoup (data, 'html.parser')
 
+us = soup.find_all('a', {
+    "href":"/currencies/bitcoin/markets/"
+})
+
+
+
+print(us[0].text)
 
